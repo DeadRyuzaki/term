bedb2ec15e2854e94106b78d9f8a89de0ac1b679
DeadRyuzaki <thammersigma_11@hotmail.com>
2022-02-08 09:48:13 +0000

    fixed Directory arguments issue

--- install.sh
+++ install.sh
@@ -6,9 +6,12 @@ if [[ $EUID -ne 0 ]]; then
    echo "Please run as sudo"
    
 else 
-   # cp /term /usr/local/sbin/
+   echo "This code only copies the term file to a better location and gives it permission to run."
+   sleep 2
    sudo cp term /usr/local/sbin/
    sudo chmod +x /usr/local/sbin/term
+
+   echo "Done, enjoy!"
 
 fi
 
